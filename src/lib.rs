c7494b04e93c7e4f672f74d76ce318e4cb6f771d
madsere <serenafederici29@gmail.com>
2026-01-26 23:39:36 +0100

    changed run_with_path into create_with_path

--- src/lib.rs
+++ src/lib.rs
@@ -13,20 +13,22 @@ pub use orchestrator::Orchestrator;
 use std::path::Path;
 
 /// Run the orchestrator with the default galaxy configuration
-pub fn run() {
+pub fn run() -> Orchestrator{
     // Initialize and start logger
     logging_utils::start_logger();
 
     let mut orchestrator = orchestrator::Orchestrator::new(Path::new("galaxy/test_galaxy.txt"));
 
     orchestrator.run();
+
+    orchestrator
 }
 
-/// Run the orchestrator with a custom galaxy file path
-pub fn run_with_path<P: AsRef<Path>>(galaxy_path: P) {
+/// Create the orchestrator with a custom galaxy file path
+pub fn create_with_path<P: AsRef<Path>>(galaxy_path: P) -> Orchestrator{
     logging_utils::start_logger();
 
-    let mut orchestrator = orchestrator::Orchestrator::new(galaxy_path.as_ref());
+    let orchestrator = orchestrator::Orchestrator::new(galaxy_path.as_ref());
 
-    orchestrator.run();
+    orchestrator
 }
